623374353e60ee8a0bbf7383eeebc5f253f37571
sina dehvari <cnadhv7@gmail.com>
2024-01-29 21:04:44 +0330

    create bookcard component and styleing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Books from "./components/Books";
 import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <Layout>
-      <h1>Book App Starting files</h1>
+      <Books />
     </Layout>
   );
 }
--- src/constants/mockData.js
+++ src/constants/mockData.js
@@ -1,3 +1,16 @@
+import img1 from "../assets/1.png";
+import img2 from '../assets/2.png';
+import img3 from '../assets/3.png';
+import img4 from '../assets/4.png';
+import img5 from '../assets/5.png';
+import img6 from '../assets/6.png';
+import img7 from '../assets/7.png';
+import img8 from '../assets/8.png';
+import img9 from '../assets/9.png';
+import img10 from '../assets/10.png';
+
+
+
 const books = [
   {
     id: "001",
